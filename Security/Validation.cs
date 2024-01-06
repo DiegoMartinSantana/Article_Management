5cd5bbe359ff9dd2dac91c95c5f81d3d24a92cf7
DiegoMartínSantana <diego.santana@alumnos.frgp.utn.edu.ar>
2024-01-05 23:50:52 -0300

    Creation Security(Admin,userSession)

---
+++ Security/Validation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+namespace Security
+{
+
+    static public class Validation
+    {
+        public static bool Login(object User)
+        {
+            Users users = User != null ? (Users)User : null;
+
+            if (users != null && users.Id != 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        public static bool IsAdmin(object User)
+        {
+
+            Users users = User != null ? (Users)User : null;
+            if (users != null)
+            {
+                return users.IsAdmin;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+
+}
